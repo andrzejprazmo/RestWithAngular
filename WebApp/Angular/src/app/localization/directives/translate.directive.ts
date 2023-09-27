4e42de9a647987ac5a13ed982085017f5b511c7f
Andrzej Prazmo <andrzej.prazmo@luxmed.pl>
2023-09-27 10:59:56 +0200

    directive final

--- WebApp/Angular/src/app/localization/directives/translate.directive.ts
+++ WebApp/Angular/src/app/localization/directives/translate.directive.ts
@@ -24,6 +24,12 @@ export class TranslateDirective {
   }
 
   setValue() {
+    if (!this.translateKey) {
+      if (this.element.nativeElement.childNodes.length > 0) {
+        const translateKeyNode = this.element.nativeElement.childNodes[0];
+        this.translateKey = translateKeyNode.wholeText;
+      }
+    }
     if (this.translateKey) {
       this.translateValue = this.store.getValue(this.translateKey);
       this.element.nativeElement.innerText = this.translateValue;
